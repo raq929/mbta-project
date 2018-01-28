e7561fde67a52021466e9398ad6c4a8ce4c040a2
racheltstevens <racheltstevens@gmail.com>
2018-01-28 16:27:58 -0500

    Create departures view
    Retrieves the csv and serves it as JSON
    Needs error handling and tests

--- departures/views.py
+++ departures/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import csv
+import requests
+from django.http import JsonResponse
 
 # Create your views here.
+from django.views import View
+
+
+class DeparturesView(View):
+  def get(self, request):
+    r = requests.get('http://developer.mbta.com/lib/gtrtfs/Departures.csv')
+    decoded_content = r.content.decode('utf-8')
+    cr = csv.DictReader(decoded_content.splitlines(), delimiter=',')
+    data_list = list(cr)
+
+    return JsonResponse({ 'data': data_list })
--- mbta/urls.py
+++ mbta/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from departures.views import DeparturesView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', DeparturesView.as_view())
 ]
