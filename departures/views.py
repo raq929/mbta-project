dae9575499eb70b5e0d056511d30a3e48cd9dc7f
racheltstevens <racheltstevens@gmail.com>
2018-01-29 12:37:31 -0500

    Handle errors from the server
    Passes the error code on to the front end.

--- departures/views.py
+++ departures/views.py
@@ -7,10 +7,19 @@ from django.views import View
 
 
 class DeparturesView(View):
-  def get(self, request):
-    r = requests.get('http://developer.mbta.com/lib/gtrtfs/Departures.csv')
-    decoded_content = r.content.decode('utf-8')
-    cr = csv.DictReader(decoded_content.splitlines(), delimiter=',')
-    data_list = list(cr)
-
-    return JsonResponse({ 'departures': data_list })
+    def get(self, request):
+        r = requests.get('http://developer.mbta.com/lib/gtrtfs/Departures.csv')
+        if r.status_code == 200:
+            decoded_content = r.content.decode('utf-8')
+            cr = csv.DictReader(decoded_content.splitlines(), delimiter=',')
+            data_list = list(cr)
+            print(data_list[0]['TimeStamp'])
+            return JsonResponse({
+                    'departures': data_list,
+                    'timeLoaded': data_list[0]['TimeStamp']
+                })
+        else:
+            return JsonResponse(
+                {'error': 'Could not load data'},
+                status=r.status_code
+            )
